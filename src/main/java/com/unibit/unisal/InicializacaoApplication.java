265321e8f8905709ffce0f46a3e75d9182132b67
vinicius moraes <viniciusmoraes243@gmail.com>
2023-04-08 15:12:35 -0300

    Foi criado a tabela Experiencia e foi alterado os gets do usuario.

--- src/main/java/com/unibit/unisal/InicializacaoApplication.java
+++ src/main/java/com/unibit/unisal/InicializacaoApplication.java
@@ -2,6 +2,7 @@ package com.unibit.unisal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class InicializacaoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InicializacaoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InicializacaoApplication.class);
 	}
 
 }
