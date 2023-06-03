3b1648a1e56d68808183fcfe15bbc7e390138895
vinicius moraes <viniciusmoraes243@gmail.com>
2023-06-03 10:30:28 -0300

    Foi implementado configurações de segurança de login.

--- src/main/java/com/unibit/unisal/InicializacaoApplication.java
+++ src/main/java/com/unibit/unisal/InicializacaoApplication.java
@@ -1,10 +1,13 @@
 package com.unibit.unisal;
 
+import com.unibit.unisal.config.ConfiguracaoWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ConfiguracaoWeb.class})
 public class InicializacaoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/unibit/unisal/config/ConfiguracaoWeb.java
@@ -0,0 +1,25 @@
+package com.unibit.unisal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class ConfiguracaoWeb implements WebMvcConfigurer {
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
